a476976b734d971c3158341a8f325bd68457b234
yanhao <haoyan@yond-e.com>
2024-05-07 09:47:49 +0800

    feat(路由)：已缓存路由二次跳转时不触发进度条

--- src/router/guard/modules/progressGuard.ts
+++ src/router/guard/modules/progressGuard.ts
@@ -1,11 +1,14 @@
 import type { Router } from 'vue-router'
 import nProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import { useMultipleTabStore } from '@/stores/multipleTab'
 
 export const createProgressGuard = (router: Router) => {
-  // TODO: 已缓存路由不再触发进度条
-  router.beforeEach(() => {
-    nProgress.start()
+  router.beforeEach((to) => {
+    const { cachedMenu } = useMultipleTabStore()
+    if (!cachedMenu.includes(to.name as string)) {
+      nProgress.start()
+    }
   })
   router.afterEach(() => {
     nProgress.done()
